1ea4dd5d69eea6f8796164f7fbd487be211699e3
Keshava Varmaa <keshava0302@gmail.com>
2021-04-18 18:10:32 +0530

    Added game Logic

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var drawer;
 var count = 0;
 var pickedWord;
 var guesses = [];
+var leaderBoard = [];
 
 var getRandomWord = () => {
   var randomIndex = Math.floor(Math.random() * words.length);
@@ -26,10 +27,16 @@ io.on("connection", (socket) => {
   io.emit("users", users);
 
   socket.on("join", (name) => {
+    if (users.findIndex((e) => e == name) > -1) {
+      return socket.emit(
+        "name error",
+        "Please choose another name. This name is aldready taken...."
+      );
+    }
     socket.name = name;
     socket.join(name);
     console.log(socket.name + " has joined. ID: " + socket.id);
-
+    leaderBoard.push({ name: name, points: 0 });
     users.push(name);
     console.log(users);
     io.emit("users", users);
@@ -45,19 +52,64 @@ io.on("connection", (socket) => {
 
   socket.on("guess word", (word) => {
     if (pickedWord.toLowerCase() == word.toLowerCase()) {
-      count = 1;
-      io.emit("guess result", `${socket.name} guessed it correctly.`);
+      guesses = [];
+      leaderBoard.forEach((element) => {
+        if (element.name === socket.name) {
+          element.points = element.points + 1;
+        }
+      });
+      io.emit("guess board", guesses);
+      console.log(users);
+      console.log(drawer, count, socket.name, users[count]);
+      if (count == users.length - 1) {
+        count = 0;
+      } else {
+        count = count + 1;
+      }
+      io.emit("guess result", {
+        correct: true,
+        msg: `${socket.name} guessed it correctly.`,
+      });
+      pickedWord = getRandomWord();
+      io.emit("clear screen", true);
+      io.sockets.in(drawer).emit("turn to guesser", "Hello");
+      drawer = users[count];
+      socket.emit("who is the drawer", {
+        drawBool: drawer == socket.name,
+        word: pickedWord,
+      });
+      io.sockets
+        .in(drawer)
+        .emit("turn to drawer", { drawBool: true, word: pickedWord });
+      console.log(drawer, count, socket.name, users[count]);
+    } else {
+      io.emit("guess result", {
+        correct: false,
+        msg: `${socket.name}'s guess: ${word}.`,
+      });
+      guesses.push({ name: socket.name, word: word });
+      io.emit("guess board", guesses);
     }
+    console.log(leaderBoard);
+    io.emit("leader board", leaderBoard);
   });
 
   socket.on("disconnect", () => {
     io.emit("message", "A User Left");
     users = users.filter((el) => el != socket.name);
+    console.log(leaderBoard, socket.name);
+    leaderBoard = leaderBoard.filter((e) => {
+      e.name != socket.name;
+    });
+    console.log(leaderBoard);
+    if (socket.name == drawer) {
+      console.log("Drawer Exit");
+    }
+    io.emit("leader board", leaderBoard);
     io.emit("users", users);
   });
 
   socket.on("draw", (obj) => {
-    console.log(obj);
     socket.broadcast.emit("draw guess", obj);
   });
 });
--- public/index.js
+++ public/index.js
@@ -4,38 +4,111 @@ var context;
 var users = [];
 var drawer = false;
 var word;
+var user = $("#username").val().trim();
+var drawerName;
+var colorDraw = "red";
 
 socket.on("draw or guess", (role) => {
   drawer = role;
+});
+
+socket.on("name error", (msg) => {
+  $(".gameScreen").hide();
+  $(".detailForm").show();
+  return alert(msg);
+});
+
+socket.on("turn to drawer", (obj) => {
+  drawer = obj.drawBool;
+  $("h5").show();
+  $("h5").text("Hint Word: " + obj.word);
+  $(".guess-div").hide();
 });
 
 socket.on("draw guess", (obj) => {
   draw(obj);
 });
 
+socket.on("clear screen", (msg) => {
+  context.clearRect(0, 0, 2000, 2000);
+});
+
+socket.on("who is the drawer", (obj) => {
+  console.log(obj.drawBool);
+  drawer = obj.drawBool;
+  console.log(obj);
+  if (obj.word && obj.drawBool) {
+    $("h5").show();
+    $("h5").text("Hint Word: " + obj.word);
+    $(".guess-div").hide();
+  }
+});
+
+socket.on("guess board", (guesses) => {
+  var content =
+    "<h4 style='text-align: left;'>Guesses Made:</h4><div class='spacer'></div>";
+  guesses.forEach((e) => {
+    content =
+      content + `<li class="list-group-item">${e.name} - ${e.word}</li>`;
+  });
+  $(".guess-board").html(content);
+});
+
+socket.on("leader board", (board) => {
+  console.log(board);
+  var content = "<h4>LeaderBoard:</h4><div class='spacer'></div>";
+  board.forEach((e) => {
+    content =
+      content + `<li class="list-group-item">${e.name} - ${e.points}</li>`;
+  });
+  $(".leader-div").html(content);
+});
+
+socket.on("turn to guesser", (msg) => {
+  $("h5").hide();
+  $(".guess-div").show();
+  drawer = false;
+});
+
 socket.on("random word", (word) => {
   word = word;
   console.log(word);
   if (word && drawer) {
-    $("h5").text(word);
+    $("h5").text("Hint Word: " + word);
     $(".guess-div").hide();
   }
 });
 
 socket.on("guess result", (b) => {
-  console.log(b);
+  if (b.correct) {
+    // alert(msg);
+    $(".result-tab").show();
+    $(".result-tab-header").text(b.msg);
+    setTimeout(() => {
+      $(".result-tab-header").hide();
+      $(".result-tab").hide();
+    }, 1000);
+  }
+  console.log(b.msg);
+  $("#guess").val("");
 });
 
 socket.on("users", (users) => {
   users = users;
-  if ($(".user-list").length == 0) {
-    $("<div/>", {
-      text: users,
-      class: "user-list",
-    }).appendTo(".gameScreen");
-  } else {
-    $(".user-list").text(users);
-  }
+  // if ($(".user-list").length == 0) {
+  //   $("<div/>", {
+  //     text: users,
+  //     class: "user-list",
+  //   }).appendTo(".gameScreen");
+  // } else {
+  //   $(".user-list").text(users);
+  // }
+  var content = "";
+  users.forEach((e) => {
+    content = content + `<li class="list-group-item">${e}</li>`;
+  });
+  console.log(content);
+  $(".user-list").html(content);
   console.log(users);
 });
 
@@ -53,6 +126,10 @@ var submitGuessForm = () => {
 var formSubmit = () => {
   $(".entryForm").submit(function (e) {
     e.preventDefault();
+    if ($("#username").val().trim() == "") {
+      alert("Please provide a name");
+      return;
+    }
     socket.emit("join", $("#username").val().trim());
 
     $(".detailForm").hide();
@@ -73,6 +150,23 @@ $(document).ready(function () {
 
   $(".detailForm").show();
   $(".gameScreen").hide();
+  $(".result-tab").hide();
+  $(".red").click(function (e) {
+    e.preventDefault();
+    colorDraw = "red";
+  });
+  $(".green").click(function (e) {
+    e.preventDefault();
+    colorDraw = "green";
+  });
+  $(".blue").click(function (e) {
+    e.preventDefault();
+    colorDraw = "blue";
+  });
+  $(".black").click(function (e) {
+    e.preventDefault();
+    colorDraw = "black";
+  });
   formSubmit();
   submitGuessForm();
 
@@ -94,8 +188,8 @@ $(document).ready(function () {
     };
 
     if (drawing == true) {
-      context.fillStyle = "red";
-      obj.color = "red";
+      context.fillStyle = colorDraw;
+      obj.color = colorDraw;
       if (drawer) {
         draw(obj);
         socket.emit("draw", obj);
